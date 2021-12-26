92ff74414b6578a568f3aa0cff15fd7536dcae89
shlee <lsw9021@snu.ac.kr>
2021-12-26 15:45:45 +0900

    x

--- sim/Environment.cpp
+++ sim/Environment.cpp
@@ -105,7 +105,7 @@ Environment()
 	mWorld->setTimeStep(1.0/(double)mSimulationHz);
 	mWorld->setGravity(Eigen::Vector3d(0,-9.81,0.0));
 
-	mSimCharacter->getSkeleton()->setSelfCollisionCheck(true);
+	mSimCharacter->getSkeleton()->setSelfCollisionCheck(false);
 	mSimCharacter->getSkeleton()->setAdjacentBodyCheck(false);
 
 	int dim_action = getDimAction();
